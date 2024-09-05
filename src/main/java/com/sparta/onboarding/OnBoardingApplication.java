4576d4fe95f4fa033970a9522e573f5b011acfbf
jiwoo <1114jiwoo@gmail.com>
2024-09-05 13:43:13 +0900

    feat ✨ : 초기 세팅

---
+++ src/main/java/com/sparta/onboarding/OnBoardingApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.onboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnBoardingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnBoardingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/onboarding/OnBoardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.onboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnBoardingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
