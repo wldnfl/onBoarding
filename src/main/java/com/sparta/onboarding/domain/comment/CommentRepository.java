aaa0d5fe0dab420f2fb9d439d593fb8e5a5bfe60
jiwoo <1114jiwoo@gmail.com>
2024-09-06 01:34:57 +0900

    🔥del : comment, schedule 파일 삭제

--- src/main/java/com/sparta/onboarding/domain/comment/CommentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.sparta.onboarding.domain.comment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
